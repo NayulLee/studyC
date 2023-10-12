4c679860d49e880e522a281d4ff5f8c8e887af06
NayulLee <144967424+NayulLee@users.noreply.github.com>
2023-10-12 15:23:56 +0900

    add input

--- 7-1/test.c
+++ 7-1/test.c
@@ -1,8 +1,12 @@
 #include <stdio.h>
 
 int main() {
+	char name[20];
 
-	printf("Hello World!");
+	printf("Hello, World!\n");
+	printf("Your Name : ");
+	scanf_s("%s", name, sizeof(name));
+	printf("Welcome, %s!", name);
 
 	return 0;
 }
