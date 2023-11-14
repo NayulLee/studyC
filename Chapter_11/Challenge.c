9c139309cea4de8c215b4c1611a72c260ae3e7dc
NayulLee <144967424+NayulLee@users.noreply.github.com>
2023-11-14 10:47:53 +0900

    Create Challenge.c

---
+++ Chapter_11/Challenge.c
@@ -0,0 +1,25 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main() {
+
+	int res, cnt = 0, max = 0;
+	char ch;
+	
+
+	while (1) {
+
+		res = scanf("%c", &ch);
+		if (res == -1) break;
+		while (ch != '\n') {
+			ch = getchar();
+			cnt++;
+		}
+		if (cnt > max) max = cnt;
+		cnt = 0;
+	}
+
+	printf("가장 긴 단어의 길이 : %d", max);
+
+	return 0;
+}
