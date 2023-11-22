a015a807a319388c78d00feba0874f23d2fa03f3
NayulLee <144967424+NayulLee@users.noreply.github.com>
2023-11-22 20:28:59 +0900

    Create practice_14.6.c

---
+++ Chapter_14/practice_14.6.c
@@ -0,0 +1,21 @@
+// 포인터 배열로 여러 개의 문자열 출력
+#include <stdio.h>
+
+int main() {
+
+	char* pary[5];
+	int i;
+
+	pary[0] = "dog";
+	pary[1] = "elephant";
+	pary[2] = "horse";
+	pary[3] = "tiger";
+	pary[4] = "lion";
+
+	for (i = 0; i < 5; i++) {
+
+		printf("%s\n", pary[i]);
+	}
+
+	return 0;
+}
