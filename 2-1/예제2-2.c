5be9bae81e2b7e8ac52a52743b16f430f5f548b8
NayulLee <144967424+NayulLee@users.noreply.github.com>
2023-11-27 09:49:33 +0900

    Create 예제2-2.c

---
+++ 2-1/예제2-2.c
@@ -0,0 +1,9 @@
+#include <stdio.h> // stdio : standard input output(표준 입출력)의 줄임말
+				   // studio.h 파일의 내용을 프로그램 안에 복사한다는 의미, C언어에서 기본으로 사용하는 입출력 함수가 들어있다.
+int main() {
+
+	printf("Be happy");  // printf함수의 기본 기능은 문자열을 화면에 출력하는 것.
+	printf("My friend");
+
+	return 0;
+}
